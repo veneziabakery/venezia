1118849ba868ff0edf4b13e3dfda060f85024ef7
Memo Buelna <admin@azsunsetgolf.com>
2013-11-05 18:50:39 -0700

    first commit

---
+++ WebContent/js/script.js
@@ -0,0 +1,37 @@
+
+	$(window).load(function() {
+			$("#search a").hoverSprite({onLoadWebSite: true});
+		});
+$(function(){
+
+
+
+ $('.box-img a').hover(
+  function(){$(this).find('img').stop().animate({opacity:'.7'}, 250)},
+  function(){$(this).find('img').stop().animate({opacity:'1'}, 250)}
+  ) 
+
+
+
+// IPad/IPhone
+	var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+    ua = navigator.userAgent,
+ 
+    gestureStart = function () {
+        viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    },
+ 
+    scaleFix = function () {
+      if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+        viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+        document.addEventListener("gesturestart", gestureStart, false);
+      }
+    };
+scaleFix();
+// Menu Android
+	var userag = navigator.userAgent.toLowerCase();
+	var isAndroid = userag.indexOf("android") > -1; 
+	if(isAndroid) {
+		$('.sf-menu').responsiveMenu({autoArrows:true});
+	}
+});
